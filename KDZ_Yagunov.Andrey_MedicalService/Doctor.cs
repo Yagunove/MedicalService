2b7ae9a4234e8527ece309a95175041a8d3e8316
Yagunov Andrey <adyagunov@edu.hse.ru>
2017-04-18 23:46:00 +0300

    Created last class "Doctor"

---
+++ KDZ_Yagunov.Andrey_MedicalService/Doctor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDZ_Yagunov.Andrey_MedicalService
+{
+
+    public class Doctor
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private string _specialty;
+       //Хирург, стоматолог, проктолог и т.д.
+        public string Specialty
+        {
+            get { return _specialty; }
+            set { _specialty = value; }
+        }
+        
+        private int _rating;
+
+        public int Rating
+        {
+            get { return _rating; }
+            set { _rating = value; }
+        }
+
+        public string Information
+        {
+            get
+            {
+                return $"{_name} - {_specialty} - {_rating}";
+            }
+        }
+
+        public Doctor(string name, string specialty, int rating)
+        {
+            _name = name;
+            _specialty = specialty;
+            _rating = rating;
+
+        }
+    }
+}
